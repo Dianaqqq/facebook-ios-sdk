36558e7ece92d17f0080fcadfda5cc4e30142c91
Diana Dobocan <diana.dobocan@linnify.com>
2021-09-15 13:44:32 +0300

    feat: remove IDFA instances

--- FBSDKCoreKit/FBSDKCoreKit/FBSDKSettings.m
+++ FBSDKCoreKit/FBSDKCoreKit/FBSDKSettings.m
@@ -327,7 +327,7 @@ FBSDKSETTINGS_PLIST_CONFIGURATION_SETTING_IMPL(
     return self.advertiserTrackingStatusBacking.unsignedIntegerValue;
   } else {
     // @lint-ignore CLANGTIDY
-    return ASIdentifierManager.sharedManager.advertisingTrackingEnabled ? FBSDKAdvertisingTrackingAllowed : FBSDKAdvertisingTrackingDisallowed;
+    return FBSDKAdvertisingTrackingDisallowed;
   }
 }
 
